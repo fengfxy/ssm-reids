168ae38e90d126579ac24b9be66b1c9bd28cdea2
fxy <1721023048@qq.com>
2018-01-29 21:28:22 +0800

    创建分支

---
+++ src/main/java/com/fxy/room/dao/RoomUserDaoImp.java
@@ -0,0 +1,37 @@
+package com.fxy.room.dao;
+
+import com.fxy.room.entity.RoomUser;
+
+public class RoomUserDaoImp implements RoomUserDao {
+
+	@Override
+	public int deleteByPrimaryKey(Integer id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insert(RoomUser roomUser) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insertSelective(RoomUser roomUser) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public RoomUser selectByPrimaryKey(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateByPrimaryKeySelective(RoomUser record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
